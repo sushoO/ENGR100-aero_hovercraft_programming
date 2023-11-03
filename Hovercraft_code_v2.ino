13abf8fa7dce5fab4c0ce215cad054b998bdcacb
Sushovit Khadka <99349457+sushoO@users.noreply.github.com>
2023-11-03 16:45:55 -0400

    Updated code to include multiple controller channels and new functions.

--- Hovercraft_code_v2.ino
+++ Hovercraft_code_v2.ino
@@ -22,8 +22,10 @@ https://cdn-learn.adafruit.com/downloads/pdf/adafruit-10-dof-imu-breakout-lsm303
 and the Examples (documentation) within the Adafruit_10DOF library
 */
 
-const int ch3Pin = 10;
-int ch3Value; // (1830 max, 920 min, 0 is 0)
+const int clyPin = 4; //ch3
+const int crxPin = 3; //ch2
+const int cryPin = 2; //ch1
+//int clyValue; // (1830 max, 920 min, 0 is 0)
 
 Servo rudderServo;
 int servoPos = 0;
@@ -31,17 +33,15 @@ int servoPos = 0;
 int sensorStatus;
 // float headings[3]; // iter 0 = angle about x-axis, iter 2 = angle about y-axis, iter 3 = angle about z-axis 
 
-const int motorPin = 11;
-int motorValue = 0;
-
-struct timerelement {
-  char* name;
-  long time;
-};
+const int liftMotorPin = 11;
+const int thrustMotorPin = 12;
+//int motorValue = 0;
 
 const int timersLength=100;
 
-timerelement timers[timersLength];
+long timers[timersLength];
+const int LIGHT = 0;
+bool lighton=false;
 
   sensors_event_t accel_event;
   sensors_event_t mag_event;
@@ -53,30 +53,29 @@ Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 
-void add_timer_value(char* name) {
-  for(int i=0;i<timersLength;i++) {
-    if(strcmp("\n",timers[i].name)!=0) {
-      strcpy(name,timers[i].name);
-      timers[i].time = 0;
-      return;
-    }
-  }
+void add_timer_value(int i) {
+//  for(int i=0;i<timersLength;i++) {
+//    if(strcmp("\n",timers[i].name)!=0) {
+//      strcpy(name,timers[i].name);
+//      timers[i].time = 0;
+//      return;
+//    }
+//  }
+    timers[i] = 0;
 }
 
-void set_timer_value(char* name, long msduration) {
-  for(int i=0;i<timersLength;i++) {
-    if(strcmp(name,timers[i].name)==0) {
-      timers[i].time = millis() + msduration;
-      return;
-    }
-  }
+void set_timer_value(int i, long msduration) {
+//  for(int i=0;i<timersLength;i++) {
+//    if(strcmp(name,timers[i].name)==0) {
+//      timers[i].time = millis() + msduration;
+//      return;
+//    }
+//  }
+    timers[i] = millis() + msduration;
 }
 
-bool check_timer(char* name) {
-  for(int i=0;i<timersLength;i++) {
-    if(strcmp(name,timers[i].name)==0) return timers[i].time <= millis() && timers[i].time != 0;
-  }
-  return false;
+bool check_timer(int i) {
+    return timers[i] <= millis() && timers[i] != 0;
 }
 
 int initSensors()
@@ -124,19 +123,43 @@ void getHeading() {
   }
 }
 
-int getController() {
-  ch3Value = pulseIn(ch3Pin, HIGH);
-  Serial.print("ch3Value = ");
-  Serial.print(ch3Value);
-  Serial.print("\n");
-  return ch3Value;
-  }
+struct controls {
+  int ly;
+  int rx;
+  int ry;
+};
 
-float readController(int controllerValue) {
+struct controls getControllerAll() {
+  controls values = {.ly = (int) pulseIn(clyPin, HIGH), .rx = (int) pulseIn(crxPin, HIGH), .ry = (int) pulseIn(cryPin, HIGH)};
+  Serial.print("clyValue = ");Serial.print(values.ly);Serial.print("\n");
+  Serial.print("crxValue = ");Serial.print(values.rx);Serial.print("\n");
+  Serial.print("cryValue = ");Serial.print(values.ry);Serial.print("\n");
+  return values;
+}
+
+int getControllerCly() {
+  int clyValue = pulseIn(clyPin, HIGH);
+  Serial.print("clyValue = ");Serial.print(clyValue);Serial.print("\n");
+  return clyValue;
+}
+
+int getControllerCrx() {
+  int crxValue = pulseIn(crxPin, HIGH);
+  Serial.print("crxValue = ");Serial.print(crxValue);Serial.print("\n");
+  return crxValue;
+}
+
+int getControllerCry() {
+  int cryValue = pulseIn(cryPin, HIGH);
+  Serial.print("cryValue = ");Serial.print(cryValue);Serial.print("\n");
+  return cryValue;
+}
+
+float controllerToMotor(int controllerValue) {
   /*The function to relate controller value to motor value is:
   f(x) = 1.80136 * 1.00271 ^ (x) - 1.80135 where x is the inputted controller value*/
   
-  motorValue = 1.80 * (pow(1.00271, controllerValue)) - 1.80;
+  int motorValue = 1.80 * (pow(1.00271, controllerValue)) - 1.80;
 
   if (motorValue <= 5) motorValue = 0;
   if (motorValue >= 255) motorValue = 255;
@@ -147,6 +170,21 @@ float readController(int controllerValue) {
   return motorValue;
 }
 
+float controllerToServo(int controllerValue) {//1900-1300
+  /*The function to relate controller value to motor value is:
+  f(x) = 1.80136 * 1.00271 ^ (x) - 1.80135 where x is the inputted controller value*/
+  
+  int servoValue = ((float)controllerValue) * 2.0f/11.0f - 1800.0f/11.0f;
+
+  if (servoValue <= 55) servoValue = 50;
+  if (servoValue >= 130) servoValue = 130;
+  
+  Serial.print("Servo Value: ");
+  Serial.print(servoValue);
+  Serial.print("\n");
+  return servoValue;
+}
+
 //==== AUTOMATIC CONTROL ====
 
 // will probably not have to use PID??? Needs testing
@@ -155,24 +193,27 @@ void findCorrections() {  // find how much we need to move in a direction to cor
   
   
   
-  }
+}
+  
 void correctiveMeasures() { // figure out what we should set servoPos/motor power to in order to fix heading
 
   
   
   
-  }
+}
 
 // ==========================
 
 void setup() {
-  pinMode(ch3Pin, INPUT);
-  rudderServo.attach(6);
-  pinMode(motorPin, OUTPUT);
+  pinMode(clyPin, INPUT);
+  pinMode(crxPin, INPUT);
+  pinMode(cryPin, INPUT);
+  rudderServo.attach(9);
+  pinMode(liftMotorPin, OUTPUT);
   Serial.begin(9600);
   sensorStatus = initSensors();
-  add_timer_value("light");
-  set_timer_value("light", 500);
+  add_timer_value(LIGHT);
+  set_timer_value(LIGHT, 500);
   pinMode(13, OUTPUT);
 }
 
@@ -181,20 +222,27 @@ void loop() {
   sensors_event_t mag_event;
   sensors_event_t bmp_event;
   sensors_vec_t   orientation;
-  
-  if(check_timer("light")) {
-    digitalWrite(13, HIGH);
-    //toggle light here
-  } else {
-    digitalWrite(13, LOW);
+
+  //flashing light
+  if(check_timer(LIGHT)) {
+    //toggle light
+    lighton = !lighton;
+    if(lighton) digitalWrite(13, HIGH);
+    else digitalWrite(13, LOW);
+    set_timer_value(LIGHT, 500);
   }
-  
+
+  //motor control
   if (sensorStatus == 1) {
     getPitchAndRoll();
     getHeading();
-    analogWrite(motorPin, readController(getController()));
+    analogWrite(liftMotorPin, controllerToMotor(getControllerCly()));
+    analogWrite(thrustMotorPin, controllerToMotor(getControllerCry()));
+    rudderServo.write(controllerToServo(getControllerCrx()));
   } else {
-    Serial.print("sensors are broken\n");
-    analogWrite(motorPin, readController(getController()));}
+    Serial.print("sensors are broken\n"); //lolol
+    analogWrite(liftMotorPin, controllerToMotor(getControllerCly()));
+    analogWrite(thrustMotorPin, controllerToMotor(getControllerCry()));
+    rudderServo.write(controllerToServo(getControllerCrx()));
+  }
 }
-
